b4c4b35791de1ecb775a79cf9cfd6d2bdd3fe5ab
FelixOgero <ogerofelix2001@gmail.com>
2025-09-22 11:49:27 +0300

    further changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import PaymentForm from './components/PaymentForm';
-import TransactionList from './components/TransactionList';
-import TransactionDetails from './components/TransactionDetails';
-import './styles/App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import PaymentForm from "./components/PaymentForm";
+import TransactionList from "./components/TransactionList";
+import TransactionDetails from "./components/TransactionDetails";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="app-container">
       <header>
         <h1>M-PESA Payment Integration</h1>
+        <a
+          href="https://github.com/FelixOgero/intasend-payment-api-integration"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            display: "block",
+            // marginBottom: "20px",
+            color: "#fff",
+            textDecoration: "none",
+            fontWeight: "bold",
+            fontSize: "16px",
+            textAlign: "center",
+          }}
+        >
+          View GitHub Repository
+        </a>
+
         <nav>
           <Link to="/">Make Payment</Link>
           <Link to="/transactions">Transactions</Link>
